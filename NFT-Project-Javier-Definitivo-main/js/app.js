330e9d7692a565aced99cb9e60000a233e68a06c
javierjpn <94402015+Truqueros@users.noreply.github.com>
2022-03-08 22:28:10 -0500

    Initial commit

---
+++ NFT-Project-Javier-Definitivo-main/js/app.js
@@ -0,0 +1,31 @@
+const select = document.getElementById('breeds');
+const card1 = document.querySelector('.card1');
+const card2 = document.querySelector('.card2'); 
+const form = document.querySelector('form');
+
+// ------------------------------------------
+//  FETCH FUNCTIONS
+// ------------------------------------------
+
+fetch('https://api.opensea.io/api/v1/assets?order_by=pk&order_direction=desc&limit=6&include_orders=true')
+  .then(response => response.json())
+  .then(data => generateImage(data.assets))
+
+function generateImage(data) {
+     const html = data.map(item =>`
+      <img src='${item.image_url}' alt>
+      <p>Collection Name: ${item.name}</p>
+      `);
+      card1.innerHTML = html[0];
+      card2.innerHTML = html[1];
+} 
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
+
+
+
+// ------------------------------------------
+//  POST DATA
+// ------------------------------------------
+
