87877d02618697a7a2f2400f334c514de96698cc
javierjpn <51091719+javierjpn@users.noreply.github.com>
2022-03-31 19:52:47 -0400

    Update app.js

--- NFT-Project-Javier-Definitivo-main/js/app.js
+++ NFT-Project-Javier-Definitivo-main/js/app.js
@@ -1,5 +1,9 @@
+const select = document.getElementById('breeds');
 const card1 = document.querySelector('.card1');
-const card2 = document.querySelector('.card2'); 
+const card2 = document.querySelector('.card2');
+const card3 = document.querySelector('.card3');
+const card4 = document.querySelector('.card4');
+const form = document.querySelector('form');
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
@@ -12,11 +16,15 @@ fetch('https://api.opensea.io/api/v1/assets?order_by=pk&order_direction=desc&lim
 function generateImage(data) {
      const html = data.map(item =>`
       <img src='${item.image_url}' alt>
-      <p>Collection Name: ${item.name}</p>
+      <p>Name: ${item.name}</p>
       `);
       card1.innerHTML = html[0];
       card2.innerHTML = html[1];
 } 
+
+fetch('https://api.opensea.io/api/v1/assets?order_by=pk&order_direction=desc&limit=6&include_orders=true')
+  .then(response => response.json())
+  .then(data => generateImage(data.assets))
 // ------------------------------------------
 //  EVENT LISTENERS
 // ------------------------------------------
